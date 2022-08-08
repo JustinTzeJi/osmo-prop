f49db3b76dcf9d71ecf66a56eca949b20e5c2e5f
Justin Koay <justintzeji@gmail.com>
2022-08-08 15:33:20 +0800

    integrate shroomdk

--- streamlit-run.py
+++ streamlit-run.py
@@ -11,7 +11,7 @@ from keyphrase_vectorizers import KeyphraseCountVectorizer
 from sklearn.decomposition import PCA
 import plotly.express as px
 import plotly.graph_objects as go
-
+from shroomdk import ShroomDK
 
 
 st.set_page_config(page_title="Attempt to classify Osmosis validators based on voting activity", page_icon="⚗️", layout="wide", menu_items={'Report a Bug': "https://github.com/JustinTzeJi/osmo-prop/issues",'About': "An experiment"})
